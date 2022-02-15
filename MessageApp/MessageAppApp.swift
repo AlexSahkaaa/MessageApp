c227f9a99b59b36c925798cd95de0165365618ac
AlexSahkaaa <TheDemmyrus@gmail.com>
2022-02-15 08:45:53 +0200

    initial commit

---
+++ MessageApp/MessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAppApp.swift
+//  MessageApp
+//
+//  Created by Alex Kolsa on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
